57457d58b2c2481641bb1edf46391be42d7da6a3
David Brady <dbrady@shinybit.com>
2012-11-26 09:07:26 -0700

    Added rackup file, updated to new Sinatra

---
+++ config.ru
@@ -0,0 +1,3 @@
+require  File.expand_path(File.join(File.dirname(__FILE__), 'echo'))
+
+run EchoApp
--- echo.rb
+++ echo.rb
@@ -1,62 +1,30 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra-websocket'
 
-set :server, 'thin'
-set :sockets, []
+class EchoApp < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+  set :server, 'thin'
+  set :sockets, []
 
-get '/' do
-  if !request.websocket?
-    erb :index
-  else
-    request.websocket do |ws|
-      ws.onopen do
-        ws.send("Hello World!")
-        settings.sockets << ws
-      end
-      ws.onmessage do |msg|
-        EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
-      end
-      ws.onclose do
-        warn("wetbsocket closed")
-        settings.sockets.delete(ws)
+  get '/' do
+    if !request.websocket?
+      erb :index
+    else
+      request.websocket do |ws|
+        ws.onopen do
+          ws.send("Hello World!")
+          settings.sockets << ws
+        end
+        ws.onmessage do |msg|
+          EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+        end
+        ws.onclose do
+          warn("wetbsocket closed")
+          settings.sockets.delete(ws)
+        end
       end
     end
   end
+
+  run! if __FILE__ == $0
 end
-
-__END__
-@@ index
-<html>
-  <body>
-  <h1>Simple Echo & Chat Server</h1>
-     <form id="form">
-       <input type="text" id="input" value="send a message"></input>
-  </form>
-     <div id="msgs"></div>
-  </body>
-
-  <script type="text/javascript">
-    window.onload = function(){
-      (function(){
-        var show = function(el){
-          return function(msg){ el.innerHTML = msg + '<br />' + el.innerHTML; }
-        }(document.getElementById('msgs'));
-
-        var ws       = new WebSocket('ws://' + window.location.host + window.location.pathname);
-        ws.onopen    = function()  { show('websocket opened'); };
-        ws.onclose   = function()  { show('websocket closed'); }
-        ws.onmessage = function(m) { show('websocket message: ' +  m.data); };
-
-        var sender = function(f){
-          var input     = document.getElementById('input');
-          input.onclick = function(){ input.value = "" };
-          f.onsubmit    = function(){
-            ws.send(input.value);
-            input.value = "send a message";
-            return false;
-          }
-        }(document.getElementById('form'));
-      })();
-    }
-  </script>
-</html>
